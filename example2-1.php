d43e9a44a6e79de4c79b8e31512f39eb8e2cd1fc
ずきん屋 <108538435+zukinshop@users.noreply.github.com>
2022-08-26 12:57:08 +0900

    Add files via upload

---
+++ example2-1.php
@@ -0,0 +1,11 @@
+<!DOCTYPE html>
+<html>
+    <head>
+        <title>例題２－１</title>
+        <meta charset="UTF-8">
+    </head>
+    <body>
+        <h1>数字のリスト</h1>
+        <p><?php echo "123<br>456<br>789"; ?></p>
+    </body>
+</html>
